f5522f916d17045469f5bcbe9aab8f3be6466512
polinayermolenko <daniel24111993@mail.ru>
2019-12-24 22:13:12 +0300

    Наследовала компонент film от абстрактного класса

--- src/components/film.js
+++ src/components/film.js
@@ -1,4 +1,4 @@
-import {createElement} from '../util.js';
+import AbstractComponent from './abstract-component.js';
 
 export const createFilmTemplate = (film) => {
   const {title, rating, releaseDate, duration, genres, poster, description, comments,
@@ -30,25 +30,14 @@ export const createFilmTemplate = (film) => {
   );
 };
 
-export default class Film {
+export default class Film extends AbstractComponent {
   constructor(film) {
+    super();
+
     this._film = film;
-    this._element = null;
   }
 
   getTemplate() {
     return createFilmTemplate(this._film);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
