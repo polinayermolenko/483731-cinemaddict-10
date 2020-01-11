75435a2f76e0a7cd98d428321f76eea517f0c8f7
polinayermolenko <daniel24111993@mail.ru>
2020-01-11 23:13:04 +0300

    Добавила логику в функцию render

--- src/utils/render.js
+++ src/utils/render.js
@@ -6,7 +6,8 @@ export const createElement = (template) => {
 
 export const RenderPosition = {
   BEFOREEND: `beforeend`,
-  AFTERBEGIN: `afterbegin`
+  AFTERBEGIN: `afterbegin`,
+  AFTEREND: `afterend`
 };
 
 export const render = (container, component, place) => {
@@ -18,10 +19,26 @@ export const render = (container, component, place) => {
     case RenderPosition.AFTERBEGIN:
       container.prepend(component.getElement());
       break;
+
+    case RenderPosition.AFTEREND:
+      container.after(component.getElement());
+      break;
   }
 };
 
 export const remove = (component) => {
   component.getElement().remove();
   component.removeElement();
+};
+
+export const replace = (newComponent, oldComponent) => {
+  const parentElement = oldComponent.getElement().parentElement;
+  const newElement = newComponent.getElement();
+  const oldElement = oldComponent.getElement();
+
+  const isElementExists = !!(parentElement && newElement && oldElement);
+
+  if (isElementExists) {
+    parentElement.replaceChild(newElement, oldElement);
+  }
 };
